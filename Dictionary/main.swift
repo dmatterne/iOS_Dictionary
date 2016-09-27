a2c6a026796d378491d52e95bb3906b50a2d24e4
David Matterne <david.matterne@gmail.com>
2016-09-27 09:38:38 +0200

    Initial Commit

---
+++ Dictionary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Dictionary
+//
+//  Created by Stannis Baratheon on 27/09/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
